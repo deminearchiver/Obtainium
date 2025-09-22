0c10150cd8baf04052cdb27f084e7d1d48d9bf2f
deminearchiver <minearchiver@gmail.com>
2025-09-22 11:55:27 +0300

    Shared exports update: - added meta package export - added CombiningBuilder widget

--- lib/flutter.dart
+++ lib/flutter.dart
@@ -20,8 +20,41 @@ export 'package:flutter/material.dart'
 
 // Third-party packages
 
+export 'package:meta/meta.dart';
 export 'package:material_symbols_icons/material_symbols_icons.dart';
 
 // Internal packages
 
 export 'package:material/material.dart';
+
+// Self-import
+
+import 'package:obtainium/flutter.dart';
+
+@immutable
+class CombiningBuilder extends StatelessWidget {
+  const CombiningBuilder({
+    super.key,
+    this.useOuterContext = false,
+    required this.builders,
+    required this.child,
+  });
+
+  final bool useOuterContext;
+
+  final List<Widget Function(BuildContext context, Widget child)> builders;
+
+  /// The child widget to pass to the last of [builders].
+  ///
+  /// {@macro flutter.widgets.transitions.ListenableBuilder.optimizations}
+  final Widget child;
+
+  @override
+  Widget build(BuildContext outerContext) {
+    return builders.reversed.fold(child, (child, buildOuter) {
+      return useOuterContext
+          ? buildOuter(outerContext, child)
+          : Builder(builder: (innerContext) => buildOuter(innerContext, child));
+    });
+  }
+}
