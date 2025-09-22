30a650965298e73cc6a404b2a6b65e4a19683c5b
deminearchiver <minearchiver@gmail.com>
2025-09-22 16:19:33 +0300

    Export LoadingIndicator from material package

--- material/lib/material.dart
+++ material/lib/material.dart
@@ -16,3 +16,5 @@ export 'src/typography/typeface_theme.dart';
 export 'src/typography/typescale_theme.dart';
 
 export 'src/elevation_theme.dart';
+
+export 'src/loading_indicator.dart';
---
+++ material/lib/src/loading_indicator.dart
@@ -0,0 +1,2 @@
+export 'package:material_loading_indicator/loading_indicator.dart';
+export 'package:material_loading_indicator/loading_indicator_theme.dart';
