354c06716ef744cd509c093099b70dba99dc2c86
deminearchiver <minearchiver@gmail.com>
2025-09-22 11:57:08 +0300

    Added LegacyThemeFactory and TypographyDefaults

---
+++ lib/theme.dart
@@ -0,0 +1,217 @@
+import 'package:obtainium/flutter.dart';
+
+abstract final class LegacyThemeFactory {
+  static ThemeData create({
+    required ColorThemeData colorTheme,
+    required TypescaleThemeData typescaleTheme,
+  }) {
+    final colorScheme = colorTheme.toLegacy();
+    return ThemeData(
+      colorScheme: colorScheme,
+      visualDensity: VisualDensity.standard,
+      splashFactory: InkSparkle.splashFactory,
+      textTheme: typescaleTheme.toBaselineTextTheme(),
+      iconTheme: IconThemeData(
+        color: colorTheme.onSurface,
+        opacity: 1.0,
+        size: 24.0,
+        opticalSize: 24.0,
+        grade: 0.0,
+        fill: 1.0,
+        weight: 400.0,
+        applyTextScaling: false,
+        shadows: const [],
+      ),
+    );
+  }
+}
+
+@immutable
+class TypographyDefaults with Diagnosticable {
+  const TypographyDefaults.from({
+    this.typeface = const TypefaceThemeDataPartial.from(),
+    this.typescale = const TypescaleThemeDataPartial.from(),
+  });
+
+  // TODO: implement TypographyDefaults.fromPlatform
+  factory TypographyDefaults.fromPlatform(TargetPlatform platform) =>
+      switch (platform) {
+        _ => const TypographyDefaults.from(),
+      };
+
+  final TypefaceThemeDataPartial typeface;
+  final TypescaleThemeDataPartial typescale;
+
+  TypographyDefaults copyWith({
+    covariant TypefaceThemeDataPartial? typeface,
+    covariant TypescaleThemeDataPartial? typescale,
+  }) {
+    if (typeface == null && typescale == null) {
+      return this;
+    }
+    return TypographyDefaults.from(
+      typeface: typeface ?? this.typeface,
+      typescale: typescale ?? this.typescale,
+    );
+  }
+
+  TypographyDefaults mergeWith({
+    TypefaceThemeDataPartial? typeface,
+    TypescaleThemeDataPartial? typescale,
+  }) {
+    if (typeface == null && typescale == null) {
+      return this;
+    }
+    return TypographyDefaults.from(
+      typeface: this.typeface.merge(typeface),
+      typescale: this.typescale.merge(typescale),
+    );
+  }
+
+  TypographyDefaults merge(TypographyDefaults? other) {
+    if (other == null) return this;
+    return mergeWith(typeface: other.typeface, typescale: other.typescale);
+  }
+
+  Widget build(BuildContext context, Widget child) {
+    return TypefaceTheme.merge(
+      data: typeface,
+      child: TypescaleTheme.merge(data: typescale, child: child),
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(
+      DiagnosticsProperty<TypefaceThemeDataPartial>(
+        "typeface",
+        typeface,
+        defaultValue: const TypefaceThemeDataPartial.from(),
+      ),
+    );
+    properties.add(
+      DiagnosticsProperty<TypescaleThemeDataPartial>(
+        "typescale",
+        typescale,
+        defaultValue: const TypescaleThemeDataPartial.from(),
+      ),
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        runtimeType == other.runtimeType &&
+            other is TypographyDefaults &&
+            typeface == other.typeface &&
+            typescale == other.typescale;
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, typeface, typescale);
+
+  static const TypographyDefaults material3Baseline = TypographyDefaults.from(
+    typeface: TypefaceThemeDataPartial.from(
+      // Roboto was the default typeface for M3 Baseline
+      brand: ["Roboto"],
+      plain: ["Roboto"],
+    ),
+  );
+
+  static const TypographyDefaults material3Expressive = TypographyDefaults.from(
+    typeface: TypefaceThemeDataPartial.from(
+      // M3 Expressive introduced variable font support
+      brand: ["Roboto Flex", "Roboto"],
+      plain: ["Roboto Flex", "Roboto"],
+    ),
+  );
+
+  static const TypographyDefaults googleMaterial3Baseline =
+      TypographyDefaults.from(
+        typeface: TypefaceThemeDataPartial.from(
+          // Google Sans (not "Flex") doesn't support ROND.
+          brand: ["Google Sans", "Roboto"],
+          plain: ["Google Sans", "Roboto"],
+        ),
+        typescale: TypescaleThemeDataPartial.from(
+          // ROND wasn't used before the introduction of GM3 Expressive.
+          // We explicitly set this axis to 0 here to avoid confusion.
+          displayLarge: TypeStylePartial.from(rond: 0.0),
+          displayMedium: TypeStylePartial.from(rond: 0.0),
+          displaySmall: TypeStylePartial.from(rond: 0.0),
+          headlineLarge: TypeStylePartial.from(rond: 0.0),
+          headlineMedium: TypeStylePartial.from(rond: 0.0),
+          headlineSmall: TypeStylePartial.from(rond: 0.0),
+          titleLarge: TypeStylePartial.from(rond: 0.0),
+          titleMedium: TypeStylePartial.from(rond: 0.0),
+          titleSmall: TypeStylePartial.from(rond: 0.0),
+          bodyLarge: TypeStylePartial.from(rond: 0.0),
+          bodyMedium: TypeStylePartial.from(rond: 0.0),
+          bodySmall: TypeStylePartial.from(rond: 0.0),
+          labelLarge: TypeStylePartial.from(rond: 0.0),
+          labelMedium: TypeStylePartial.from(rond: 0.0),
+          labelSmall: TypeStylePartial.from(rond: 0.0),
+          displayLargeEmphasized: TypeStylePartial.from(rond: 0.0),
+          displayMediumEmphasized: TypeStylePartial.from(rond: 0.0),
+          displaySmallEmphasized: TypeStylePartial.from(rond: 0.0),
+          headlineLargeEmphasized: TypeStylePartial.from(rond: 0.0),
+          headlineMediumEmphasized: TypeStylePartial.from(rond: 0.0),
+          headlineSmallEmphasized: TypeStylePartial.from(rond: 0.0),
+          titleLargeEmphasized: TypeStylePartial.from(rond: 0.0),
+          titleMediumEmphasized: TypeStylePartial.from(rond: 0.0),
+          titleSmallEmphasized: TypeStylePartial.from(rond: 0.0),
+          bodyLargeEmphasized: TypeStylePartial.from(rond: 0.0),
+          bodyMediumEmphasized: TypeStylePartial.from(rond: 0.0),
+          bodySmallEmphasized: TypeStylePartial.from(rond: 0.0),
+          labelLargeEmphasized: TypeStylePartial.from(rond: 0.0),
+          labelMediumEmphasized: TypeStylePartial.from(rond: 0.0),
+          labelSmallEmphasized: TypeStylePartial.from(rond: 0.0),
+        ),
+      );
+
+  static const TypographyDefaults
+  googleMaterial3Expressive = TypographyDefaults.from(
+    typeface: TypefaceThemeDataPartial.from(
+      // The ROND axis is currently only available for Google Sans Flex,
+      // making it a no-op for most of the other possibly installed fonts.
+      // This particular information was ripped from a file
+      // located at the path "/product/etc/fonts_customization.xml"
+      // on a Google Pixel with Android 16 QPR1 beta 3 (Material 3 Expressive)
+      brand: ["Google Sans Flex", "Roboto Flex", "Google Sans", "Roboto"],
+      plain: ["Google Sans Flex", "Roboto Flex", "Google Sans", "Roboto"],
+    ),
+    typescale: TypescaleThemeDataPartial.from(
+      displayLarge: TypeStylePartial.from(rond: 0.0),
+      displayMedium: TypeStylePartial.from(rond: 0.0),
+      displaySmall: TypeStylePartial.from(rond: 0.0),
+      headlineLarge: TypeStylePartial.from(rond: 0.0),
+      headlineMedium: TypeStylePartial.from(rond: 0.0),
+      headlineSmall: TypeStylePartial.from(rond: 0.0),
+      titleLarge: TypeStylePartial.from(rond: 0.0),
+      titleMedium: TypeStylePartial.from(rond: 0.0),
+      titleSmall: TypeStylePartial.from(rond: 0.0),
+      bodyLarge: TypeStylePartial.from(rond: 0.0),
+      bodyMedium: TypeStylePartial.from(rond: 0.0),
+      bodySmall: TypeStylePartial.from(rond: 0.0),
+      labelLarge: TypeStylePartial.from(rond: 0.0),
+      labelMedium: TypeStylePartial.from(rond: 0.0),
+      labelSmall: TypeStylePartial.from(rond: 0.0),
+      displayLargeEmphasized: TypeStylePartial.from(rond: 100.0),
+      displayMediumEmphasized: TypeStylePartial.from(rond: 100.0),
+      displaySmallEmphasized: TypeStylePartial.from(rond: 100.0),
+      headlineLargeEmphasized: TypeStylePartial.from(rond: 100.0),
+      headlineMediumEmphasized: TypeStylePartial.from(rond: 100.0),
+      headlineSmallEmphasized: TypeStylePartial.from(rond: 100.0),
+      titleLargeEmphasized: TypeStylePartial.from(rond: 100.0),
+      titleMediumEmphasized: TypeStylePartial.from(rond: 100.0),
+      titleSmallEmphasized: TypeStylePartial.from(rond: 100.0),
+      bodyLargeEmphasized: TypeStylePartial.from(rond: 100.0),
+      bodyMediumEmphasized: TypeStylePartial.from(rond: 100.0),
+      bodySmallEmphasized: TypeStylePartial.from(rond: 100.0),
+      labelLargeEmphasized: TypeStylePartial.from(rond: 100.0),
+      labelMediumEmphasized: TypeStylePartial.from(rond: 100.0),
+      labelSmallEmphasized: TypeStylePartial.from(rond: 100.0),
+    ),
+  );
+}
